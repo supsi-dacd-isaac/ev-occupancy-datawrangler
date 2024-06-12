a2576cdde1cbe229034333a812b5b7c52e4894f5
nepslor <nepslor@gmail.com>
2024-06-12 15:46:12 +0200

    renamed as package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ev-occupancy-datawrangler",
+    name="evoccupancydatawrangler",
     version="0.1",
     author="lorenz",
     author_email="lorenz@lorenz",
